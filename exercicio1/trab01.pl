4a9fcb0af20dcfdc0766011ff3cbf4d32684d2e8
Felipe Santos <fdms@icomp.ufam.edu.br>
2018-03-29 15:33:32 -0400

    macaco derrotado

--- exercicio1/trab01.pl
+++ exercicio1/trab01.pl
@@ -1,7 +1,7 @@
 
 
  
-move(state( meio, sobreCaixa, meio, semBanana), pega, state(meio, sobreCaixa,meio,comBanana)).    
+move(state(meio, sobreCaixa, meio, semBanana), pega, state(meio, sobreCaixa,meio,comBanana)).    
 move(state( P, noChao, P, Has),sobe,state( P, sobreCaixa, P, Has)).
 move(state( P1, noChao, P1, Has),arrasta( P1, P2),state( P2, noChao, P2, Has)).
 move(state( P1, noChao, B, Has),anda( P1, P2),state( P2, noChao, B, Has)).
@@ -13,12 +13,26 @@ podePegar(State1) :-
     podePegar(State2).
 
 
+podePegarlist(state(_,_,_, comBanana), Como):-
+    Como = [].
+podePegarlist(state(meio, sobreCaixa, meio, semBanana),Como):-
+    Como = [pega].
+podePegarlist(state(meio, noChao, meio, semBanana),Como):-
+    Como = [sobe, pega].
+podePegarlist(state(naJanela, noChao, naJanela, semBanana),Como):-
+    Como = [arrasta(naJanela, meio), sobe, pega].
+podePegarlist(state(naPorta, noChao, naJanela, semBanana),Como):-
+    Como = [anda(naPorta, naJanela), arrasta(naJanela, meio), sobe, pega].
+    
 
-podePegarComo(state(_,_,_, comBanana), Como).
-podePegarComo(State1,Como):-
-    move(State1,Move,State2),
-    podePegarComo(State2, parcialComo),
-    add(Move, parcialComo, Como).
+podePegarComo(State, [], Como):-
+    podePegarlist(State, Como).
+podePegarComo(State, Inicio, Como):-
+    Inicio = [X|L],
+    podePegar(State),
+    move(State, X, State2),
+    podePegarComo(State2, L, Como).
+    
 
-add(X,L,[X|L]).
+
 
