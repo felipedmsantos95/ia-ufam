0a73157d6cd23e9a211d47dbcf4d1b2780292cc7
Felipe Santos <fdms@icomp.ufam.edu.br>
2018-04-27 11:25:20 -0400

    q3 ok

--- exercicio2/q2.pl
+++ exercicio2/q2.pl
@@ -11,9 +11,6 @@ retira([(A,B)|Tail],[A|Result]):-
 
 qualEstado(mesa([(A,B)|T1],[(C,D)|T2],[(E,F)|T3],[(G,H)|T4]),Result):-
 	retira([(A,B),(C,D),(E,F),(G,H)],Result).
-
-
-formataEstado([X1, X2, X3, X4], estado(X1, X2, X3, X4)):-!.
      
 
    
--- exercicio2/q3.pl
+++ exercicio2/q3.pl
@@ -2,10 +2,29 @@ member(X, [X|_]).
 member(X, [_|T]):-
     member(X,T).
 
-intercessao([],_,[]).
-intercessao([X|T1], B, [X|T3]):-
-    member(X,B),
-    intercessao(T1, B, T3).
-intercessao([_|T1], B, T2):-
-    intercessao(T1,B
+memberP((X1,_), estado(X1,_,_,_)).
+memberP((X1,_), estado(_,X1,_,_)).
+memberP((X1,_), estado(_,_,X1,_)).
+memberP((X1,_), estado(_,_,_,X1)).
+memberP((_,X2), estado(X2,_,_,_)).
+memberP((_,X2), estado(_,X2,_,_)).
+memberP((_,X2), estado(_,_,X2,_)).
+memberP((_,X2), estado(_,_,_,X2)).
+memberP((X1,_), estado((X1,X1),_,_,_)).
+memberP((X1,_), estado(_,(X1,X1),_,_)).
+memberP((X1,_), estado(_,_,(X1,X1),_)).
+memberP((X1,_), estado(_,_,_,(X1,X1))).
+memberP((_,X2), estado((X2,X2),_,_,_)).
+memberP((_,X2), estado(_,(X2,X2),_,_)).
+memberP((_,X2), estado(_,_,(X2,X2),_)).
+memberP((_,X2), estado(_,_,_,(X2,X2))).
+
+jogaveis([],_,[]).
+jogaveis([X|T1], B, [X|T3]):-
+    memberP(X,B),
+    jogaveis(T1, B, T3).
+jogaveis([_|T1], B, T2):-
+    jogaveis(T1,B,T2).
+
+
 
