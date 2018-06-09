d4e5abc5d2cd9608868bbf64d5ce60a858a8050c
Guilherme Peña Céspedes <cespedesgp@ufam.edu.br>
2018-06-08 20:27:19 -0400

    functor leitura(In)
    Segunda definição da função leitura e regra de processamento.

--- exercicio3/dcg2.pl
+++ exercicio3/dcg2.pl
@@ -4,20 +4,42 @@ read_sentence(L):-
     atomic_list_concat(L, ' ', A).
 
 % =====================
-regra([]):-!.
-regra([jogo|B]):-
-	write(B).
-regra([branco|B]):-.
-regra([duque|B]):-.
-regra([terno|B]):-.
-regra([quadra|B]):-.
-regra([quina|B]):-.
-regra([sena|B]):-.
+regra([]):-
+	Z =.. Y,!.
+regra([jogo|B],Y,Z):-
+	append([,],[],Y),
+	regra(B,Y,Z),!.
+regra([e|B],Y,Z):-
+	append(Y,[],W),
+	regra(B,W,Z),!.
+regra([branco|B]):-
+	append(Y,[0],W),
+	regra(B,W,Z),!.
+regra([as|B],Y,Z):-
+	append(Y,[1],W),
+	regra(B,W,Z),!.
+regra([um|B],Y,Z):-
+	append(Y,[1],W),
+	regra(B,W,Z),!.
+regra([duque|B],Y,Z):-
+	append(Y,[2],W),
+	regra(B,W,Z),!.
+regra([terno|B],Y,Z):-
+	append(Y,[3],W),
+	regra(B,W,Z),!.
+regra([quadra|B],Y,Z):-
+	append(Y,[4],W),
+	regra(B,W,Z),!.
+regra([quina|B],Y,Z):-
+	append(Y,[5],W),
+	regra(B,W,Z),!.
+regra([sena|B],Y,Z):-
+	append(Y,[6],W),
+	regra(B,W,Z),!.
 
 % O que eu preciso é que a leitura retorne no Out uma lista [,,1,2]
 % caso a pesso escreva "...jogo.. branco e duque ...." usando a
 % regra().
-leitura([]):-!.
-leitura(L,Out):-
-	regra(L,Out),
-	leitura(Out,B).
+leitura(In):-
+	read_sentence(X),
+	regra(X,_,In).
