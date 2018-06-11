d88eca9e2d04f56944790f671baa6a5712ff7f91
Guilherme Peña Céspedes <cespedesgp@ufam.edu.br>
2018-06-11 10:18:46 -0400

    sou gay

--- exercicio3/dcg2.pl
+++ exercicio3/dcg2.pl
@@ -4,42 +4,56 @@ read_sentence(L):-
     atomic_list_concat(L, ' ', A).
 
 % =====================
-regra([]):-
-	Z =.. Y,!.
+regra([],Y,Z):-
+	Z =.. Y.
+
+regra([equipe|B],Y,Z):-
+	append([],[],Y),
+	regra(B,Y,Z).
+
+regra(['1'|_],_,1):-!.
+
 regra([jogo|B],Y,Z):-
 	append([,],[],Y),
-	regra(B,Y,Z),!.
+	regra(B,Y,Z).
+
 regra([e|B],Y,Z):-
 	append(Y,[],W),
-	regra(B,W,Z),!.
-regra([branco|B]):-
+	regra(B,W,Z).
+
+regra([branco|B],Y,Z):-
 	append(Y,[0],W),
-	regra(B,W,Z),!.
+	regra(B,W,Z).
+
 regra([as|B],Y,Z):-
 	append(Y,[1],W),
-	regra(B,W,Z),!.
-regra([um|B],Y,Z):-
-	append(Y,[1],W),
-	regra(B,W,Z),!.
+	regra(B,W,Z).
+
 regra([duque|B],Y,Z):-
 	append(Y,[2],W),
-	regra(B,W,Z),!.
+	regra(B,W,Z).
+
 regra([terno|B],Y,Z):-
 	append(Y,[3],W),
-	regra(B,W,Z),!.
+	regra(B,W,Z).
+
 regra([quadra|B],Y,Z):-
 	append(Y,[4],W),
-	regra(B,W,Z),!.
+	regra(B,W,Z).
+
 regra([quina|B],Y,Z):-
 	append(Y,[5],W),
-	regra(B,W,Z),!.
+	regra(B,W,Z).
+
 regra([sena|B],Y,Z):-
 	append(Y,[6],W),
-	regra(B,W,Z),!.
+	regra(B,W,Z).
 
 % O que eu preciso é que a leitura retorne no Out uma lista [,,1,2]
 % caso a pesso escreva "...jogo.. branco e duque ...." usando a
 % regra().
+%leitura([]):-!.
 leitura(In):-
+	nl,
 	read_sentence(X),
 	regra(X,_,In).
