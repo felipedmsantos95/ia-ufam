60aab5f24b90f7cd295be9583552efad44c11e92
Felipe Santos <fdms@icomp.ufam.edu.br>
2018-06-15 21:52:39 -0400

    organizando o código

--- exercicio3/saudacao.pl
+++ exercicio3/saudacao.pl
@@ -3,17 +3,15 @@ read_sentence(L):-
     atom_codes(A, Cs), 
     atomic_list_concat(L, ' ', A).
 
-oracao(J1,J2) --> frase(J1),[e],frase(J2).
+oracao(J1,J2) --> frase(J1),conjuncao,frase(J2).
 
 frase(J1) --> sujeito, predicado(J1).
 
 sujeito --> pronomePessoal(_).
-sujeito --> pronomePossessivo(G), substComum(G).
+sujeito --> pronomePossessivo(G), substComum(G).%para o caso da frase que se refere ao segundo parceiro
 
-%predicado(J1) --> verbo, substProprio(G, J1), conjuncao, artigo(G), pronomePossessivo(G), substComum(G), verboP, substProprio(G, J2).
-
-predicado(J1) --> verbo ,artigo(G),substProprio(G, J1).
-redicado(J1) --> verbo ,substProprio(-_, J1).
+predicado(J1) --> verbo, artigo(G),substProprio(G, J1).
+predicado(J1) --> verbo, substProprio(_, J1).
 
 artigo(m) --> [o].
 artigo(f) --> [a].
@@ -29,18 +27,16 @@ substProprio(f, X) --> [X], {member(X, [carlla, fernanda, roberta, alice, floren
 
 verbo --> [me, chamo] | [sou] | [se, chama] | [eh].
 
-bemVindo:-
-    write('quem sao vcs?'), nl,
+leituraDupla:-
+    write('Baré: Bem vindo ao mundo Baré!'),nl,
+    write('Baré: Por favor entre com o nome da sua dupla, na ordem de jogada.'), nl,
     write('>> '),
     read_sentence(S),
     oracao(J1,J2,S,[]),
-    write('Bem vindos'),
+    write('Bem vindos '),
     write(J1),
     write(' e '),
-    write(J2).
+    write(J2), write('! Escolham das equipes abaixo:').
 
-leituraDupla:-
-    nl,
-	read_sentence(X).
-    
+
 
